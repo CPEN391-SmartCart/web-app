8af1e176c964edfa0eda91fb87b5e4d600e94347
Tawsif Hasan <tawsifhasan1998@yahoo.com>
2021-03-24 00:08:33 -0700

    minor changes

--- routes.js
+++ routes.js
@@ -39,24 +39,32 @@ function listSectionsByLegendId(req, res) {
 function listSectionsByStoreId(req, res) {
     console.log("GET /sections/{{storeId}}")
     pool.query("SELECT * FROM legends WHERE store_id = $1", [req.params.storeId], function (err, legend_ids) {
+        console.log("\nTest here 1\n")
+
         if (err) {
             res.status(400).send(err)
             return
         }
 
-        console.log(legend_ids.rows)
+        console.log("\nTest here 2\n")
 
         for(var i = 0; i < legend_ids.rows.length; i++){
             var legend_id = legend_ids.rows[i].id
+            console.log("\nTest here 3\n")
+
             pool.query("SELECT * FROM sections WHERE legend_id = $1", [legend_id], function (err, result) {
                 if (err) {
                     res.status(400).send(err)
                     return
                 }
                 legend_ids.rows[i].sections = result.rows
+                console.log("\nTest here 4\n")
+
             })
         }
 
+        console.log("\nTest here 5\n")
+        
         res.status(200).send(legend_ids.rows)
         return
     })
